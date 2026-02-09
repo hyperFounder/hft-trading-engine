f1109b5ced2d360b4065128c9c3d2c7e212c02ec
hyperFounder <ribeiro.ryan@protonmail.com>
2026-02-09 17:32:40 +0000

    Add Idempotent Advice

--- src/main/java/com/fintech/trading/advice/Idempotent.java
+++ src/main/java/com/fintech/trading/advice/Idempotent.java
@@ -1,4 +1,12 @@
 package com.fintech.trading.advice;
 
-public class Idempotent {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Idempotent {
+    String headerName() default "X-Idempotency-Key";
 }
